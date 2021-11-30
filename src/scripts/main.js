829af50e448b999e4fdc6bc2df0297b0276038e3
Ogur4iK <koryavi2014@mail.ru>
2021-12-01 02:23:07 +0300

    adding tabs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,27 @@ import Swiper, { Navigation } from 'swiper';
 
 Swiper.use([Navigation]);
 
+function createTabs(tabsSelector, contentsSelector) {
+    const tabs = document.querySelectorAll(tabsSelector);
+    const contents = document.querySelectorAll(contentsSelector);
+    tabs[0].classList.add('active');
+    contents[0].classList.add('active');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            const tabTarget = this.getAttribute('data-tab');
+            const content = document.querySelector(tabTarget);
+            tabs.forEach(tab => {
+                tab.classList.remove('active');
+            });
+            contents.forEach(content => {
+                content.classList.remove('active');
+            });
+            content.classList.add('active');
+            this.classList.add('active');
+        });
+    });
+}
+
 window.onload = function () {
     const swiperServices = new Swiper('.services-slider', {
         slidesPerView: 3,
@@ -24,17 +45,10 @@ window.onload = function () {
     });
 
 
-    let openBtns = document.querySelectorAll('#open-menu-services');
-    let closeBtns = document.querySelectorAll('#close-service-menu');
-    let menu = document.querySelector('.services-menu');
-    let body = document.querySelector('body');
-    openBtns.forEach(btn => {
-        btn.addEventListener('click', function () {
-            menuOpen();
-            history.pushState(null, document.title, window.location.pathname + '#');
-        });
-    });
-
+    const openBtns = document.querySelectorAll('#open-menu-services');
+    const closeBtns = document.querySelectorAll('#close-service-menu');
+    const menu = document.querySelector('.services-menu');
+    const body = document.querySelector('body');
     function menuOpen() {
         menu.classList.add('open');
         body.classList.add('no-scroll');
@@ -48,14 +62,20 @@ window.onload = function () {
         menu.style.paddingLeft = getScrollWidth() + 'px';
     }
     function getScrollWidth() {
-        let div = document.createElement('div');
+        const div = document.createElement('div');
         div.style.overflowY = 'scroll';
         div.style.width = '50px';
         document.body.append(div);
-        let scrollWidth = div.offsetWidth - div.clientWidth;
+        const scrollWidth = div.offsetWidth - div.clientWidth;
         div.remove();
         return scrollWidth;
     }
+    openBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            menuOpen();
+            history.pushState(null, document.title, window.location.pathname + '#');
+        });
+    });
 
     closeBtns.forEach(btn => {
         btn.addEventListener('click', function () {
@@ -65,4 +85,8 @@ window.onload = function () {
     });
 
     window.addEventListener("hashchange", menuClose);
+
+    createTabs('.tab-top-btn', '.tab-top-content');
+    createTabs('.tab-bottom-btn', '.tab-bottom-content');
+    createTabs('.result-tab', '.result-tab-content');
 }
